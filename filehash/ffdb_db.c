175db338cd6c61323a4357ac79989184888f1bab
cthomas0 <c.e.thomas@damtp.cam.ac.uk>
2026-03-13 14:45:10 +0000

    Update ffdb_db.c
    Fixed bug: added name to parameter in __ffdb_dberr function

--- filehash/ffdb_db.c
+++ filehash/ffdb_db.c
@@ -73,7 +73,7 @@ ffdb_dbopen(const char* fname, int flags, int mode, const void* openinfo)
 }
 
 static int
-__ffdb_dberr (const FFDB_DB *, ...)
+__ffdb_dberr (const FFDB_DB *thedb, ...)
 {
   return (FFDB_ERROR);
 }
